c1637522f20b91229aa8248808189b1071692445
DankBomber <azubyk12@gmail.com>
2022-02-01 12:03:13 -0500

    Login/Signup screens

---
+++ lib/login.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:tot_tracker/signup.dart';
+import 'package:tot_tracker/main.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      //Appbar is used to have a back arrow at top of screen when logging in
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios,
+            size: 30,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      //Container holds all elements on screen
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text("Login",
+                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20,),
+                    Text("Login to your account",
+                      style: TextStyle(fontSize: 15, color: Colors.grey[700]),
+                    )
+                  ],
+                ),
+                //Adds text fields so use can input info
+                //Calls function below to create the widget
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    children: <Widget>[
+                      inputFile(label: "Email"),
+                      inputFile(label: "Password", obscureText: true)
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  //Adds a border around the login button
+                  child: Container(
+                    padding: EdgeInsets.only(top: 1, left: 1),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border(
+                          bottom: BorderSide(color: Colors.black),
+                          top: BorderSide(color: Colors.black),
+                          left: BorderSide(color: Colors.black),
+                          right: BorderSide(color: Colors.black),
+                        )
+                    ),
+                    //Material Button is used to make the login button on screen
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60,
+                      //When pressed it brings you to home screen nav page
+                      onPressed: () {
+                        //Navigator.push(context, MaterialPageRoute(builder: (context) => Nav()));
+                      },
+                      color: Color(0xff0095FF),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Text("Login",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                //Text below login button
+                //User can press "Sign up" and it brings them to sign up page
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Don't have an account?  "),
+                    new GestureDetector(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
+                      },
+                      child: new Text("Sign up",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            color: Color(0xff0095FF)
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                //Container is used to add image to bottom of screen
+                Container(
+                  padding: EdgeInsets.only(top: 100),
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/background.png"),
+                        fit: BoxFit.fitHeight
+                    ),
+                  ),
+                )
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//Function that is used to create a TextField for user to input data
+Widget inputFile({label, obscureText = false})
+{
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w400,
+            color: Colors.black87
+        ),
+      ),
+      SizedBox(height: 5,),
+      TextField(
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: (Colors.grey[400])!,
+              ),
+            ),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: (Colors.grey[400])!,)
+            )
+        ),
+      ),
+      SizedBox(height: 10,)
+    ],
+  );
+}
---
+++ lib/signup.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:tot_tracker/login.dart';
+import 'package:tot_tracker/main.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.white,
+      //Appbar is used to have a back arrow at top of screen when signing up in
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios,
+            size: 30,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      //Will allow user to scroll is screen size is too small to display all boxes
+      body: SingleChildScrollView(
+        //Container holds all elements on screen
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          height: MediaQuery.of(context).size.height - 50,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              //Top two rows of text on screen
+              Column(
+                children: <Widget>[
+                  Text("Sign up",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Text("",
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey[700]
+                    ),
+                  )
+                ],
+              ),
+              //Creates text fields that the user can input their data on
+              Column(
+                children: <Widget>[
+                  inputFile(label: "Full Name"),
+                  inputFile(label: "Email"),
+                  inputFile(label: "Password", obscureText: true),
+                  inputFile(label: "Confirm Password", obscureText: true),
+                ],
+              ),
+              //Container for the sign up buttom
+              Container(
+                padding: EdgeInsets.only(top: 1, left: 1),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border(
+                      bottom: BorderSide(color: Colors.black),
+                      top: BorderSide(color: Colors.black),
+                      left: BorderSide(color: Colors.black),
+                      right: BorderSide(color: Colors.black),
+                    )
+                ),
+                //Material Button is used to make the sign up button on screen
+                child: MaterialButton(
+                  minWidth: double.infinity,
+                  height: 60,
+                  onPressed: () {
+                    //Navigator.push(context, MaterialPageRoute(builder: (context) => Nav()));
+                  },
+                  color: Color(0xff0095FF),
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Text("Sign up",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              //Text below sign up button
+              //User can press "Login" and it brings them to Login page
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Already have an account?  "),
+                  new GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                    },
+                    child: new Text("Login",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Color(0xff0095FF)
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//Function that is used to create a TextField for user to input data
+Widget inputFile({label, obscureText = false})
+{
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w400,
+            color: Colors.black87
+        ),
+      ),
+      SizedBox(height: 5,),
+      TextField(
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: (Colors.grey[400])!,
+              ),
+            ),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: (Colors.grey[400])!,)
+            )
+        ),
+      ),
+      SizedBox(height: 10,)
+    ],
+  );
+}
