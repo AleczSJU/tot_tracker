fd9114591b3a306bf2228a8f1836f86e559af82f
DankBomber <azubyk12@gmail.com>
2022-02-23 16:12:53 -0500

    Working on connection to mariaDB server

--- lib/classrooms.dart
+++ lib/classrooms.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:tot_tracker/calendar.dart';
+import 'package:mysql1/mysql1.dart';
 
 class ClassroomsPage extends StatefulWidget {
   const ClassroomsPage({Key? key}) : super(key: key);
@@ -8,16 +10,51 @@ class ClassroomsPage extends StatefulWidget {
   _ClassroomsPageState createState() => _ClassroomsPageState();
 }
 
-class _ClassroomsPageState extends State<ClassroomsPage> {
-  CalendarFormat _calendarFormat = CalendarFormat.week;
-  DateTime _focusedDay = DateTime.now();
-  DateTime? _selectedDay;
+class _ClassroomsPageState extends State<ClassroomsPage> with TickerProviderStateMixin {
+  List<Tab> _tabs = [];
+  List<Widget> _tabViews = [];
+  List<String> tabNames = [];
+  var numTabs = 0;
+  late TabController _tabController;
+
+  Future<void> getTabs() async {
+    var settings = new ConnectionSettings(
+      host: '68.82.13.214',
+      port: 3306,
+      user: 'alecRemote',
+      password: 'alecZubyk12',
+      db: 'tot_tracker',
+    );
+    var conn = await MySqlConnection.connect(settings);
+    var tabs = await conn.query('select name from classroom;');
+
+    for (var row in tabs) {
+      tabNames.add('${row[0]}');
+      print('${row[0]}');
+      numTabs++;
+    }
+
+    for (var i in tabNames) {
+      _tabs.add(new Tab(child: Text(i),));
+      _tabViews.add(new Scaffold(body: Calendar()));
+    }
+  }
+
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: _tabs.length);
+  }
+
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 8,
-      child: Scaffold(
+    return Scaffold(
+      //length: numTabs,
+      //child: Scaffold(
           appBar: AppBar(
             centerTitle: true,
             leading: BackButton(
@@ -25,113 +62,72 @@ class _ClassroomsPageState extends State<ClassroomsPage> {
               onPressed: () => Navigator.of(context).pop(),
             ),
             title: Text('Classrooms'),
-            bottom: PreferredSize(
-                child: TabBar(
-                    isScrollable: true,
-                    unselectedLabelColor: Colors.white.withOpacity(0.3),
-                    indicatorColor: Colors.white,
-                    tabs: [
-                      Tab(
-                        child: Text('Angelfish'),
-                      ),
-                      Tab(
-                        child: Text('Starfish'),
-                      ),
-                      Tab(
-                        child: Text('Seahorses'),
-                      ),
-                      Tab(
-                        child: Text('Narwhals'),
-                      ),
-                      Tab(
-                        child: Text('Sharks'),
-                      ),
-                      Tab(
-                        child: Text('Sea Otters'),
-                      ),
-                      Tab(
-                        child: Text('Sea Turtles'),
-                      ),
-                      Tab(
-                        child: Text('Sea Dragons'),
-                      )
-                    ]),
-                preferredSize: Size.fromHeight(30.0)),
+            bottom: TabBar(
+                controller: _tabController,
+                isScrollable: true,
+                unselectedLabelColor: Colors.white.withOpacity(0.3),
+                indicatorColor: Colors.white,
+                //tabs: List<Widget>.generate(_tabs.length, (int index)),
+                tabs: _tabs,
+                // tabs: [
+                //   Tab(
+                //     child: Text('Angelfish'),
+                //   ),
+                //   Tab(
+                //     child: Text('Starfish'),
+                //   ),
+                //   Tab(
+                //     child: Text('Seahorses'),
+                //   ),
+                //   Tab(
+                //     child: Text('Narwhals'),
+                //   ),
+                //   Tab(
+                //     child: Text('Sharks'),
+                //   ),
+                //   Tab(
+                //     child: Text('Sea Otters'),
+                //   ),
+                //   Tab(
+                //     child: Text('Sea Turtles'),
+                //   ),
+                //   Tab(
+                //     child: Text('Sea Dragons'),
+                //   )
+                // ]
+            ),
           ),
           body: TabBarView(
-            children: <Widget>[
-              Scaffold(
-                body: Center(
-                  child: TableCalendar(
-                    firstDay: DateTime.utc(2010, 10, 16),
-                    lastDay: DateTime.utc(2030, 3, 14),
-                    focusedDay: _focusedDay,
-                    calendarFormat: _calendarFormat,
-                    selectedDayPredicate: (day) {
-                      return isSameDay(_selectedDay, day);
-                    },
-                    onDaySelected: (selectedDay, focusedDay) {
-                      if (!isSameDay(_selectedDay, selectedDay)) {
-                        setState(() {
-                          _selectedDay = selectedDay;
-                          _focusedDay = focusedDay;
-                        });
-                      }
-                    },
-                    onFormatChanged: (format) {
-                      if (_calendarFormat != format) {
-                        setState(() {
-                          _calendarFormat = format;
-                        });
-                      }
-                    },
-                    onPageChanged: (focusedDay) {
-                      _focusedDay = focusedDay;
-                    },
-                  ),
-                ),
-                floatingActionButton: FloatingActionButton(
-                  child: Icon(Icons.add),
-                  onPressed: () {  },
-                ),
-              ),
-              Container(
-                child: Center(
-                  child: Text('Tab 2'),
-                ),
-              ),
-              Container(
-                child: Center(
-                  child: Text('Tab 3'),
-                ),
-              ),
-              Container(
-                child: Center(
-                  child: Text('Tab 4'),
-                ),
-              ),
-              Container(
-                child: Center(
-                  child: Text('Tab 5'),
-                ),
-              ),
-              Container(
-                child: Center(
-                  child: Text('Tab 6'),
-                ),
-              ),
-              Container(
-                child: Center(
-                  child: Text('Tab 7'),
-                ),
-              ),
-              Container(
-                child: Center(
-                  child: Text('Tab 8'),
-                ),
-              ),
-            ],
-          )),
+            controller: _tabController,
+            children: _tabViews,
+            // children: <Widget>[
+            //   Scaffold(
+            //     body: Calendar(),
+            //   ),
+            //   Scaffold(
+            //     body: Calendar(),
+            //   ),
+            //   Scaffold(
+            //     body: Calendar(),
+            //   ),
+            //   Scaffold(
+            //     body: Calendar(),
+            //   ),
+            //   Scaffold(
+            //     body: Calendar(),
+            //   ),
+            //   Scaffold(
+            //     body: Calendar(),
+            //   ),
+            //   Scaffold(
+            //     body: Calendar(),
+            //   ),
+            //   Scaffold(
+            //     body: Calendar(),
+            //   ),
+            // ],
+          )
+      //),
     );
   }
 }
